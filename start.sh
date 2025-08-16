6afff7df048bd694e2eb435b9c0db88abff304c4
KingNekoVN <minhgit20@gmail.com>
2025-08-16 16:18:13 +0700

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Check ảo hóa
+virtu=$(egrep -i '^flags.*(vmx|svm)' /proc/cpuinfo | wc -l)
+if [ $virtu = 0 ] ; then echo -e "[Error] ${RED}Virtualization/KVM in your Server/VPS is OFF\nExiting...${NC}";
+else
+
+#Start VM
+sudo qemu-system-x86_64 -vnc :0 -hda w10x64.img -smp cores=2 -m 8096M -machine usb=on -device usb-tablet
+
+# Start noVNC
+websockify --web=/novnc 5901 localhost:5900 &
+
+echo "================================================"
+echo " 🖥️  VNC: http://localhost:6080/vnc.html"
+echo " Supported Code Sandbox (use ngrok or cloudflare)"
+echo " Code By MinhNeko and Quang Huy(Github) Youtube: https://youtube.com/@MinhNeko | 
+echo "================================================"
+
