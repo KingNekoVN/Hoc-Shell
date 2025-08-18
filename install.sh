80968920f7411f139883f9511c7fe40c03856bb1
KingNekoVN <minhgit20@gmail.com>
2025-08-18 09:01:48 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,20 +11,22 @@ apt-get update && apt-get install -y --no-install-recommends \
     unzip \
     p7zip-full \
     wget \
+    gunzip \
     && rm -rf /var/lib/apt/lists/* && apt clean
 
 # Tạo thư mục bên trong noVNC
 mkdir -p /data /novnc /opt/qemu /cloud-init
 
-# Tải Micro 10
-wget -P /opt/qemu https://archive.org/download/astro-os-11-24-h-2/AstroOS%2011%2024H2.iso
-qemu-img create -f qcow2 disk.img 110M
+# Tải Windows2012r2
+qemu-img create -f raw 2012r2.img 10G
+wget -O- --no-check-certificate http://drive.muavps.net/windows/Windows2012r2.gz | gunzip | dd of=2012r2.img
 
 # Setup noVNC
 curl -L https://github.com/novnc/noVNC/archive/refs/tags/v1.3.0.zip -o /tmp/novnc.zip && \
 unzip /tmp/novnc.zip -d /tmp && \
 mv /tmp/noVNC-1.3.0/* /novnc && \
 rm -rf /tmp/novnc.zip /tmp/noVNC-1.3.0
+
 # Cài đặt ngrok
 curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
   | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
@@ -32,8 +34,10 @@ curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
   | sudo tee /etc/apt/sources.list.d/ngrok.list \
   && sudo apt update \
   && sudo apt install ngrok
+
 # Token Ngrok
-echo "Bấm terminal và dán token của bạn"
+ngrok config add-authtoken 2xB1jLlRuHFhvqas7ZDdc4K8G23_4iTdz9zAYYFk3K2YGtiNL
+
 # Tải file start.sh để bắt đầu khởi chạy
 wget -O start.sh https://github.com/KingNekoVN/Hoc-Shell/raw/refs/heads/main/start.sh
 clear
