d67e8e97f7c733f5cf13cdb2dae1766d4f414c45
KingNekoVN <minhgit20@gmail.com>
2025-08-18 10:56:13 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+# Kết quả
+echo "================================================"
+echo " 🖥️  VNC: http://localhost:6080"
+echo " Use ngrok to http(port 6080) or use tailscale and paste ip in your browser"
+echo " Code By MinhNeko and Quang Huy(Github) Youtube: youtube.com/@MinhNeko | youtube.com/@huyhuy-o2z
+echo "================================================"
+
 #Start VM
 sudo qemu-system-x86_64 -vnc :0 -hda 2012r2.img -smp cores=2 -m 8096M -machine usb=on -device usb-tablet
 
-# Start noVNC
-websockify --web=/novnc 5901 localhost:5900 &
+#Start noVNC
+websockify --web=/novnc 6080 localhost:5900 &
 
-echo "================================================"
-echo " 🖥️  VNC: http://iptailscale:6080"
-echo " Use ngrok to http(port 6080) or use tailscale"
-echo " Code By MinhNeko and Quang Huy(Github) Youtube: https://youtube.com/@MinhNeko | 
-echo "================================================"
 
