83c65a8213809c2e73c37fa174c4771e9115e40f
KingNekoVN <minhgit20@gmail.com>
2025-08-21 16:41:05 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 echo "Code By MinhNeko"
+echo "Lưu ý là phải dùng cpu mới cài driver được"
 # Tải package bổ sung vào để tải
 apt-get update && apt-get install -y --no-install-recommends \
-    qemu-system-x86-64 \
+    qemu-kvm \
     cloud-image-utils \
-    novnc \
-    websockify \
     python3-pip \
     unzip \
     p7zip-full \
@@ -16,15 +15,14 @@ apt-get update && apt-get install -y --no-install-recommends \
 # Tạo thư mục bên trong noVNC
 mkdir -p /data /novnc /opt/qemu /cloud-init
 
-# Tải Windows2012r2
-qemu-img create -f raw 2012r2.img 60G
-wget -O- --no-check-certificate http://drive.muavps.net/windows/Windows2012r2.gz | gunzip | dd of=2012r2.img
+# Tải Windows
+wget -O win.iso https://go.microsoft.com/fwlink/p/?LinkID=2195404&clcid=0x409&culture=en-us&country=US
 
-# Setup noVNC
-curl -L https://github.com/novnc/noVNC/archive/refs/tags/v1.3.0.zip -o /tmp/novnc.zip && \
-unzip /tmp/novnc.zip -d /tmp && \
-mv /tmp/noVNC-1.3.0/* /novnc && \
-rm -rf /tmp/novnc.zip /tmp/noVNC-1.3.0
+# Tải driver
+wget -O virtio.iso https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/archive-virtio/virtio-win-0.1.271-1/virtio-win-0.1.271.iso
+
+# Tạo disk
+qemu-img create -f qcow2 disk.qcow2 90G
 
 # Cài đặt ngrok
 curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
@@ -34,28 +32,32 @@ curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
   && sudo apt update \
   && sudo apt install ngrok
 
-# Cài tailscale(nếu cần)
-curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/noble.noarmor.gpg | sudo tee /usr/share/keyrings/tailscale-archive-keyring.gpg >/dev/null
-curl -fsSL https://pkgs.tailscale.com/stable/ubuntu/noble.tailscale-keyring.list | sudo tee /etc/apt/sources.list.d/tailscale.list
-sudo apt-get install tailscale
-
-# Connect colab với tailscale
- sudo tailscale up
-
 # Token Ngrok
 ngrok config add-authtoken 2xB1jLlRuHFhvqas7ZDdc4K8G23_4iTdz9zAYYFk3K2YGtiNL
 
 # Khởi chạy VPS
 sudo qemu-system-x86_64 \
-  -m 8G \
-  -cpu EPYC \
-  -boot order=d \
-  -drive file=2012r2.img \
-  -device usb-ehci,id=usb,bus=pci.0,addr=0x4 \
-  -device usb-tablet \
-  -vnc :0 \
-  -cpu n270 \
-  -smp sockets=1,cores=4,threads=2 \
-  -device e1000,netdev=n0 -netdev user,id=n0 \
-  -accel tcg,thread=multi \
-
+-M q35,nvdimm=on,hmat=on \
+-usb -device usb-tablet -device usb-kbd \
+-cpu qemu64,+ssse3,+sse4.1,+sse4.2,+aes,+xsave,+xsaveopt,+xsavec,+xgetbv1,+avx,+avx2,+fma,+fma4,+popcnt,+cx16,+mmx,+sse,+sse2 \
+-smp sockets=1,cores=8,threads=1 \
+-m 8384M \
+-object memory-backend-memfd,id=mem,size=8384M,share=on,prealloc=on \
+-machine memory-backend=mem \
+-object iothread,id=iothread0 \
+-drive file=disk.qcow2,if=none,format=qcow2,cache=unsafe,aio=threads,discard=on,id=hd0 \
+-device virtio-blk-pci,drive=hd0,iothread=iothread0 \
+-drive file=win.iso,media=cdrom,if=none,id=cdrom0 \
+-device ahci,id=ahci0 \
+-device ide-cd,drive=cdrom0,bus=ahci0.0 \
+-drive file=virtio.iso,media=cdrom,if=none,id=cdrom1 \
+-device ide-cd,drive=cdrom1,bus=ahci0.1 \
+-device qxl-vga,vgamem_mb=2048,ram_size=268435456,vram_size=268435456 \
+-device virtio-balloon,id=balloon0 \
+-netdev user,id=n0,restrict=off \
+-device virtio-net-pci,netdev=n0 \
+-accel tcg,thread=multi,tb-size=34777216,split-wx=on \
+-rtc clock=host,base=utc \
+-boot order=d,menu=on \
+-display vnc=:0 \
+-msg timestamp=on
